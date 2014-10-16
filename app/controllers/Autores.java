23b5b53d821d9ace478f2800b85b8afdaf1ba1da
Rene Quisaguano <renequisaguano@gmail.com>
2014-10-16 14:19:15 -0500

    Probando archivos ignorados

---
+++ app/controllers/Autores.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class Autores extends Controller {
+
+    public static void index() {
+        render();
+    }
+
+    public static void create(){
+    	render();
+    }
+    
+
+
+}
--- app/models/Autor.java
+++ app/models/Autor.java
@@ -6,9 +6,18 @@ import play.db.jpa.*;
 
 
 @Entity
+
 public class Autor extends Model{
 	private String nombre;
 	private String apellido;
 	private String edad;
+
+
+public Autor(String nombre, String apellido, String edad){
+	this.nombre=nombre;
+	this.apellido=apellido;
+	this.edad=edad;
+}
+
 
 }
