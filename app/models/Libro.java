3867dba20a013920535b9b339002383624f5c2cc
edgar-ach <edgar.acurio@gmail.com>
2014-10-16 11:49:49 -0500

    Modelo libro
    con many to one

---
+++ app/models/Libro.java
@@ -0,0 +1,14 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+ 
+import play.db.jpa.*;
+@Entity
+public class Libro extends Model{
+	public String titulo;
+	public String numeroPaginas;
+	public String categoria;
+	@ManyToOne
+	public Autor autor;	
+}
